e3d2be6f8953c6ea24c8926be3d09f42ea416468
Aaron Winter <winter.aaron93@gmail.com>
2021-01-20 10:15:43 +0100

    split up input function

--- src/player.rs
+++ src/player.rs
@@ -34,49 +34,85 @@ pub fn input(
             let to_coords = *coords + direction;
 
             if room.is_exit(to_coords) {
-                despawn::prepare_despawn(commands, &mut active_entities);
+                change_room(
+                    commands,
+                    &images,
+                    &grid,
+                    &mut state,
+                    &mut cameras,
+                    &mut active_entities,
+                    direction,
+                    *coords,
+                    &mut tween,
+                )
+            } else {
+                match room.objects.get(&to_coords) {
+                    Some(bobs) => {
+                        handle_bobs(&grid, bobs, from_coords, to_coords, &mut tween);
+                    }
+                    None => {
+                        if let Some(tile) = room.tiles.get(&to_coords) {
+                            if *tile == TileType::Floor {
+                                tween.from = grid.map_to_world(from_coords);
+                                tween.to = grid.map_to_world(to_coords);
+                                tween.start(0.15, TweenMode::Move);
 
-                state.change_current_room(direction);
-
-                let mut camera_transform = cameras.iter_mut().next().unwrap();
-
-                let room = state.get_current_room();
-
-                dungeon::spawn_room(commands, &grid, &images, room);
-
-                camera_transform.translation = grid.map_to_world(room.center());
-
-                tween.from = grid.map_to_world(from_coords);
-                tween.to = grid.map_to_world(from_coords);
-
-                tween.start(0.15, TweenMode::Move);
-            } else if let Some(tile) = room.tiles.get(&(to_coords)) {
-                if *tile == TileType::Floor {
-                    match room.objects.get(&to_coords) {
-                        Some(vec) => {
-                            for bob in vec {
-                                match bob {
-                                    BoardObject::Player => {}
-                                    BoardObject::Enemy(enemy) => {
-                                        tween.from = grid.map_to_world(from_coords);
-                                        tween.to = grid.map_to_world(to_coords);
-                                        tween.start(0.15, TweenMode::Attack);
-                                    }
-                                    BoardObject::Item(item) => {
-                                        // item collection logic
-                                    }
-                                }
+                                *coords = to_coords;
                             }
-                        }
-                        None => {
-                            tween.from = grid.map_to_world(from_coords);
-                            tween.to = grid.map_to_world(to_coords);
-                            tween.start(0.15, TweenMode::Move);
-
-                            *coords = to_coords;
                         }
                     }
                 }
+            }
+        }
+    }
+}
+
+pub fn change_room(
+    commands: &mut Commands,
+    images: &Images,
+    grid: &Grid,
+    state: &mut GameState,
+    cameras: &mut Query<&mut Transform, With<Camera>>,
+    active_entities: &mut Query<Entity, With<Active>>,
+    direction: Coordinates,
+    coords: Coordinates,
+    tween: &mut Tween,
+) {
+    despawn::prepare_despawn(commands, active_entities);
+
+    state.change_current_room(direction);
+
+    let mut camera_transform = cameras.iter_mut().next().unwrap();
+
+    let room = state.get_current_room();
+
+    dungeon::spawn_room(commands, &grid, &images, room);
+
+    camera_transform.translation = grid.map_to_world(room.center());
+
+    tween.from = grid.map_to_world(coords);
+    tween.to = grid.map_to_world(coords);
+
+    tween.start(0.15, TweenMode::Move);
+}
+
+pub fn handle_bobs(
+    grid: &Grid,
+    bobs: &Vec<BoardObject>,
+    from_coords: Coordinates,
+    to_coords: Coordinates,
+    tween: &mut Tween,
+) {
+    for bob in bobs {
+        match bob {
+            BoardObject::Player => {}
+            BoardObject::Enemy(enemy) => {
+                tween.from = grid.map_to_world(from_coords);
+                tween.to = grid.map_to_world(to_coords);
+                tween.start(0.15, TweenMode::Attack);
+            }
+            BoardObject::Item(item) => {
+                // item collection logic
             }
         }
     }
