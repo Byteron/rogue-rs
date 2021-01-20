847a4a2056f1fb4c57ca1bb778a269cc99ce16df
Aaron Winter <winter.aaron93@gmail.com>
2021-01-20 09:04:31 +0100

    version bump to bevy master

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 mod core;
 mod dungeon;
+mod tween;
 mod player;
 mod rogue;
 
-use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
-use bevy::diagnostic::PrintDiagnosticsPlugin;
 use bevy::prelude::*;
 use rogue::Rogue;
 
 fn main() {
     App::build()
-        // .add_plugin(FrameTimeDiagnosticsPlugin)
-        // .add_plugin(PrintDiagnosticsPlugin::default())
         .add_plugin(Rogue)
         .run();
 }
